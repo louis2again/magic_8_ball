4631ef1f32c380b38a7e70c4798a477be4b77bee
louis again <louis2again@hotmail.com>
2020-04-30 14:11:30 +0100

    playing around with header fonts and size

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,52 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.lightBlue,
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: Colors.blue.shade900,
+          title:
+              Text('Ask Me Anything', style: TextStyle(fontFamily: 'Pacifico')),
+          centerTitle: true,
+        ),
+        body: BallPage(),
       ),
-    );
+    ),
+  );
+}
+
+class BallPage extends StatefulWidget {
+  @override
+  _BallPageState createState() => _BallPageState();
+}
+
+class _BallPageState extends State<BallPage> {
+  int ballNumber = 1;
+
+  void changeBallFace() {
+    setState(() {
+      ballNumber = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Row(
+      children: <Widget>[
+        Expanded(
+          child: FlatButton(
+            onPressed: () {
+              changeBallFace();
+            },
+            child: Image.asset('images/ball$ballNumber.png'),
+          ),
+        ),
+      ],
+    ));
+  }
+}
